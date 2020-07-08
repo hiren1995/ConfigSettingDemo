e7d52fcc66f8fd80fc6f7d55f21ad86957cdc09b
Hiren Kadam <hiren.kadam@logicalwings.com>
2020-07-08 12:39:24 +0530

    Initial Commit

---
+++ ConfigSettingsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConfigSettingsDemo
+//
+//  Created by Hiren Kadam on 08/07/20.
+//  Copyright © 2020 Hiren Kadam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
